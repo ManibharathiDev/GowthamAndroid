1afeba63b2605ec9f5a2b772d160fca63e940f67
Manibharathi <manibharath159@gmail.com>
2024-11-11 21:18:23 +0530

    Constraint Layout Design

--- app/src/main/java/com/gowtham/androidtraining/MainActivity.java
+++ app/src/main/java/com/gowtham/androidtraining/MainActivity.java
@@ -1,6 +1,8 @@
 package com.gowtham.androidtraining;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void btnMovieClick(View view) {
+//        Toast.makeText(this, "Movie Clicked", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, TicketActivity.class);
+        intent.putExtra("title", "Batman");
+        intent.putExtra("desc", "Action Thriller Movie");
+        startActivity(intent);
 
     }
 }
---
+++ app/src/main/java/com/gowtham/androidtraining/TicketActivity.java
@@ -0,0 +1,32 @@
+package com.gowtham.androidtraining;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ticket);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        TextView movieName = findViewById(R.id.tvTitle);
+        movieName.setText(title);
+
+    }
+}
